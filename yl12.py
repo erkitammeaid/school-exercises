8dcaa234c3ec17b4218d517f1efa6c796d72236f
erkitammeaid <55434301+erkitammeaid@users.noreply.github.com>
2019-10-25 10:53:19 +0300

    Add files via upload

---
+++ yl12.py
@@ -0,0 +1,17 @@
+fruits = ["arbuus", "pirn", "apelsin"]
+print (fruits [0])
+fruits = ["arbuus", "pirn", "apelsin", "kirss"]
+print (fruits[-1])
+fruits[1] = "laim"
+print(fruits)
+if "õun" in fruits:
+    print("Õun on nimekirjas")
+else:
+    print("Õun ei ole nimekirjas")
+print(len(fruits))
+fruits.remove("apelsin")
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.sort()
+print(fruits)

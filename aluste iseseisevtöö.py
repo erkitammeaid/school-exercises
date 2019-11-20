33bdd3e2ef912984aea3bc48077fb9ab2325b334
Palderjan <55434301+erkitammeaid@users.noreply.github.com>
2019-11-20 12:42:24 +0200

    Add files via upload

---
+++ aluste iseseisevtöö.py
@@ -0,0 +1,27 @@
+ferrari = {
+    "nimetus": "ferrari",
+    "mudel": "458",
+    "väljalaske aasta":2009,
+    "värvus": "punane"
+}
+
+lada = {
+    "nimetus": "lada",
+    "mudel": "112",
+    "väljalaske aasta":1999,
+    "värvus": "roheline"
+}
+
+bmw = {
+    "nimetus": "bmw",
+    "mudel": "e28",
+    "väljalaske aasta": 1981,
+    "värvus": "valge"
+}
+
+for y in ferrari:
+    print(ferrari["nimetus"])
+    print(ferrari["mudel"])
+    print(ferrari["väljalaske aasta"])
+    print(ferrari["värvus"])
+    break
